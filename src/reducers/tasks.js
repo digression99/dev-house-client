2051d8124a6009548d6f1a301938d79975abb681
Pseudocoder Kim <pseudocoder@Pseudocoderui-MacBook-Pro.local>
2018-08-15 23:41:58 +0900

    receive user info reducer remove

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -3,7 +3,7 @@ import { RECEIVE_TASKS, RECEIVE_USER_INFO } from "../actions/constants";
 export default (state = [], action) => {
     switch (action.type) {
         case RECEIVE_TASKS:
-        case RECEIVE_USER_INFO :
+        // case RECEIVE_USER_INFO :
             return [...state, ...action.payload.tasks.tasks];
         default:
             return state;
