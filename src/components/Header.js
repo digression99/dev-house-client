233b7ad78cb1992a921155ea6e2e6005eb1e1f0a
Pseudocoder Kim <pseudocoder@Pseudocoderui-MacBook-Pro.local>
2018-08-15 20:44:29 +0900

    add login header button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import {getUsername} from "../selectors";
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Header extends Component {
-
     render() {
         return (
             <div>
                 <nav>
                     <div className="nav-wrapper">
                         <a href="#" className="brand-logo">Dev House</a>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
+                            <li>
+                                {
+                                    this.props.username ? <a href="https://www.naver.com">Logout</a>
+                                        : <a href="https://www.google.com">Login</a>
+                                }
+                            </li>
+                        </ul>
                     </div>
                 </nav>
             </div>
@@ -15,4 +25,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+const mapStateToProps = state => ({
+    username : getUsername(state)
+});
+
+export default connect(mapStateToProps, actions)(Header);
